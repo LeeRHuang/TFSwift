fb537677309a8ccb89acb98dd9196fc0717e4b17
tianfeng888888 <lirihuang@126.com>
2014-12-04 17:12:39 +0800

    Initial Commit

---
+++ TFSwift1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TFSwift1
+//
+//  Created by lirihuang on 14-12-4.
+//  Copyright (c) 2014年 TF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
